28ff641127bbdf30d03013efb3cf86ae7e0d8314
Prathab <jm.prathab@gmail.com>
2017-11-22 23:55:01 +0530

    Attempt to test postgresql connectivity
    * Extend the test cases to test CRUD operations on database

---
+++ database/src/test/java/com/prathab/data/postgresql/TestPostgresqlConnectivity.java
@@ -0,0 +1,37 @@
+package com.prathab.data.postgresql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.testng.annotations.Test;
+
+public class TestPostgresqlConnectivity {
+
+  @Test
+  public void testPostgresqlInsert() {
+
+    System.out.println("-------- PostgreSQL JDBC Connection Testing --------");
+
+    try {
+      Class.forName("org.postgresql.Driver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+      System.out.println("Postgresql java driver cannot be found");
+    }
+    Connection connection = null;
+
+    try {
+      connection = DriverManager.getConnection(
+          "jdbc:postgresql://127.0.0.1:5432/shopping", "shopping_user",
+          "jaihanuman");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    if (connection != null) {
+      System.out.println("You made it, take control your database now!");
+    } else {
+      System.out.println("Failed to make connection!");
+    }
+  }
+}
