f2fd2aa390f708e9a40f95ae9c7d4938925df53c
Prathab <jm.prathab@gmail.com>
2017-12-18 00:16:27 +0530

    New Stress test class
    * Added a new Stress test. This will test the web service by making 100 calls simultaneously from different okhttp clients

--- api/src/test/java/com/prathab/api/shopping/resources/TestProductsResource.java
+++ api/src/test/java/com/prathab/api/shopping/resources/TestProductsResource.java
@@ -39,4 +39,44 @@ public class TestProductsResource {
 
 		Assert.assertEquals(response.code(), 200);
 	}
+	
+	@Test (priority = 1)
+	public void stressTestProductsReadBulk() throws IOException {
+		System.out.println("Stress Testing Products resource fetch bulk");
+		
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					testStressProductsReadBulk();
+				} catch (IOException e) {
+					e.printStackTrace();
+					Assert.assertTrue(false,"Stress test failed");
+				}
+			}
+			
+		};
+		
+		for(int i= 0 ; i< 200 ; i++) {
+			new Thread(runnable).start();
+		}
+	}
+	
+	public void testStressProductsReadBulk() throws IOException {
+		OkHttpClient okHttpClient = new OkHttpClient.Builder().connectTimeout(15, TimeUnit.SECONDS)
+				.readTimeout(15, TimeUnit.SECONDS).writeTimeout(15, TimeUnit.SECONDS).build();
+		
+		HttpUrl.Builder urlBuilder = HttpUrl.parse(ENDPOINT + "/products").newBuilder();
+		urlBuilder.addQueryParameter("page", "1");
+		urlBuilder.addQueryParameter("limit", "50");
+
+		Request request = new Request.Builder().url(urlBuilder.build()).get().build();
+
+		Response response = okHttpClient.newCall(request).execute();
+
+		Assert.assertEquals(response.code(), 200);
+		System.out.println("Success : " + Thread.currentThread().getName());
+		
+	}
 }
